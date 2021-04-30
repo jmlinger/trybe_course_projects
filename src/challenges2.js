d7c86aa28b440d515e0503c270e08503ade129d5
jmlinger <jmlinger10@gmail.com>
2021-04-30 12:09:29 -0300

    resolve requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,16 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, name) {
+  let techObject = {};
+  let techObjectList = [];
+  for(let nameTech of tech.sort()) {
+    techObject["tech"] = nameTech;
+    techObject["name"] = name;
+    techObjectList.push(techObject);  
+  }
+  return techObjectList;
 }
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'John'));
 
 // Desafio 11
 function generatePhoneNumber() {
