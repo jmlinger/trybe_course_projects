6bc9887c9fee56596673d073633ab1a4115f943e
jmlinger <jmlinger10@gmail.com>
2021-05-05 19:36:28 -0300

    resolve requisito 3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let arrayColor = ['blue', 'red', 'yellow', 'purple'];
+//adiciona cores à paleta.
+let arrayColor = ['black', 'red', 'yellow', 'purple'];
 for (let index = 0; index < 4; index += 1) {
   document.getElementsByClassName("color")[index].style.backgroundColor = arrayColor[index];
 }
+
