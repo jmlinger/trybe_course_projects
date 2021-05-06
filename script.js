bb1087726ed73df8346d8b0fc7f485e260c59cfe
jmlinger <jmlinger10@gmail.com>
2021-05-05 21:51:17 -0300

    resolve requisito 4

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ for (let index = 0; index < 4; index += 1) {
   document.getElementsByClassName("color")[index].style.backgroundColor = arrayColor[index];
 }
 
+//cria tabela pixel board.
+let pixelBoard = document.getElementById("pixel-board");
+let createTable = document.createElement('table');
+for (let i = 0; i < 5; i += 1) {
+  let createRow = createTable.insertRow(0);
+  for (let j = 0; j < 5; j += 1) {
+    let createCell = createRow.insertCell(j);
+  }
+  createRow = '';
+  createCell = '';
+}
+pixelBoard.appendChild(createTable);
+
+//configura a tabela da pixel board.
+let table = document.getElementsByTagName('table');
+let td = document.getElementsByTagName('td');
+table[0].style.width = 600 + "px";
+table[0].style.height = 600 + "px";
+for (i = 0; i < 25; i += 1) {
+  td[i].style.border = 1 + 'px' + ' ' + 'solid' + ' ' + 'black';
+  td[i].style.backgroundColor = "white";
+  td[i].classList.add('pixel');
+}
