233315c14e534dde79021207c3f14fb0087429a3
jmlinger <jmlinger10@gmail.com>
2021-05-06 21:18:11 -0300

    resolve requisito 7 e 8

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let buttonAdd = document.querySelector('#criar-tarefa');
-let inputText = document.querySelector('#texto-tarefa');
-let taskList = document.querySelector('#lista-tarefas');
-function taskAdd () {
-  let itemLista = document.createElement('li');
-  itemLista.innerText = inputText.value;
-  taskList.appendChild(itemLista);
+// adiciona texto a lista ao clicar no botao Adicionar;
+const buttonAdd = document.querySelector('#criar-tarefa');
+const inputText = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
+function taskAdd() {
+  const createItemList = document.createElement('li');
+  createItemList.innerText = inputText.value;
+  taskList.appendChild(createItemList);
   inputText.value = '';
 }
 buttonAdd.addEventListener('click', taskAdd);
+
+const itemList = document.getElementsByTagName('li');
+function printColorItemList(event) {
+  for (let index = 0; index < itemList.length; index += 1) {
+    itemList[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+taskList.addEventListener('click', printColorItemList);
