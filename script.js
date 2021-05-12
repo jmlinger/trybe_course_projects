115c5a156bd9c558dd843d111ee62ec0febdc9fe
jmlinger <jmlinger10@gmail.com>
2021-05-12 14:53:36 -0300

    resolve restante dos requisitos

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
+function randomColors(opacity = 1) {
+  let r = Math.random() * 255;
+  let g = Math.random() * 255;
+  let b = Math.random() * 255;
+  return `rgba( ${r}, ${g}, ${b}, ${opacity})`;
+}
+
 // adiciona cores à paleta.
 const color = document.getElementsByClassName('color');
-const arrayColor = ['black', 'red', 'yellow', 'purple'];
-for (let index = 0; index < 4; index += 1) {
-  color[index].style.backgroundColor = arrayColor[index];
+color[0].style.backgroundColor = 'black';
+for (let index = 1; index < 4; index += 1) {
+  color[index].style.backgroundColor = randomColors();
 }
 
-// cria tabela pixel board.
-const pixelBoard = document.getElementById('pixel-board');
+// Cria e configura botão e input para criar pixelboard.
+const createInput = document.createElement('input');
+const createButton = document.createElement('button');
+const secaoBotoes = document.getElementById('botoes');
+createInput.id = 'board-size';
+createButton.id = 'generate-board';
+createButton.innerText = 'VQV';
+secaoBotoes.appendChild(createInput);
+secaoBotoes.appendChild(createButton);
+
+// cria tabela pixelboard.
+const buttonGenerateBoard = document.getElementById('generate-board');
 const createTable = document.createElement('table');
-for (let i = 0; i < 5; i += 1) {
-  const createRow = createTable.insertRow(0);
-  for (let j = 0; j < 5; j += 1) {
-    createRow.insertCell(j);
+const pixelBoard = document.getElementById('pixel-board');
+function createPixelBoardConditions() {
+  pixelBoard.innerHTML = null;
+  boardSize = document.getElementById('board-size').value;
+  if (boardSize < 5 && boardSize > 0) {
+    boardSize = 5;
+  } if (boardSize > 50) {
+    boardSize = 50;
+  } if (boardSize == '') {
+    alert('Board inválido!');
   }
+  createPixelBoard();
 }
-pixelBoard.appendChild(createTable);
 
-// configura a tabela da pixel board.
-const cell = document.getElementsByTagName('td');
-for (let i = 0; i < cell.length; i += 1) {
-  cell[i].style.width = '40px';
-  cell[i].style.height = '40px';
-  cell[i].style.border = '1px solid black';
-  cell[i].style.backgroundColor = 'white';
-  cell[i].classList.add('pixel');
+function createPixelBoard() {
+  for (let i = 0; i < boardSize; i += 1) {
+    const createRow = createTable.insertRow(0);
+    for (let j = 0; j < boardSize; j += 1) {
+      createRow.insertCell(j);
+    }
+  }
+  pixelBoard.appendChild(createTable);
+  document.getElementById('board-size').value = null;
+  configuraPixelBoard();
+}
+buttonGenerateBoard.addEventListener('click', createPixelBoardConditions);
+  
+// configura a tabela da pixelboard.
+function configuraPixelBoard() {
+  const cell = document.getElementsByTagName('td');
+  for (let i = 0; i < cell.length; i += 1) {
+    cell[i].style.width = '40px';
+    cell[i].style.height = '40px';
+    cell[i].style.border = '1px solid black';
+    cell[i].style.backgroundColor = 'white';
+    cell[i].classList.add('pixel');
+  }
+  printColor();
 }
 
 // adiciona e remove classe selected nas paletcolors quando clicadas. Referência: ADRIANA BIBERG.
@@ -38,21 +77,26 @@ for (let indexSelect = 0; indexSelect < color.length; indexSelect += 1) {
 }
 
 // adiciona cor aos pixels.
-const pixel = document.querySelectorAll('.pixel');
-function printColor(selecionado) {
-  const clicado = selecionado;
-  const selected = document.querySelector('.selected');
-  clicado.target.style.backgroundColor = selected.style.backgroundColor;
-}
-for (let indexPixels = 0; indexPixels < pixel.length; indexPixels += 1) {
-  pixel[indexPixels].addEventListener('click', printColor);
+function printColor() {
+  pixel = document.querySelectorAll('.pixel');
+  function print(selecionado) {
+    const clicado = selecionado;
+    const selected = document.querySelector('.selected');
+    clicado.target.style.backgroundColor = selected.style.backgroundColor;
+  }
+  for (let indexPixels = 0; indexPixels < pixel.length; indexPixels += 1) {
+    pixel[indexPixels].addEventListener('click', print);
+  }
+  cleanBoard();
 }
 
-// configura botão.
-const button = document.querySelector('#clear-board');
+// configura botão para limpar board.
 function cleanBoard() {
-  for (let indexPixels = 0; indexPixels < pixel.length; indexPixels += 1) {
-    pixel[indexPixels].style.backgroundColor = 'white';
+  const button = document.querySelector('#clear-board');
+  function clean() {
+    for (let indexPixels = 0; indexPixels < pixel.length; indexPixels += 1) {
+      pixel[indexPixels].style.backgroundColor = 'white';
+    }
   }
+  button.addEventListener('click', clean);
 }
-button.addEventListener('click', cleanBoard);
