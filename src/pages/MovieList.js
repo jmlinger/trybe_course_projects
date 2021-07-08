99fac153baafff2d78f2d943125b5b0ca0524472
jmlinger <jmlinger10@gmail.com>
2021-07-08 19:06:51 -0300

    Resolve requisito 2: criou-se uma funçao assincrona para lidar com a requisiçao simulada de movies, assim como uma estrutura condicional para aparecer Loading enquanto a requisiçao nao fora completada. Algumas alteraçoes para passar no lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+import { MovieCard, Loading } from '../components';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.simulatedFetchMovies = this.simulatedFetchMovies.bind(this);
+    this.renderMovieList = this.renderMovieList.bind(this);
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.simulatedFetchMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  async simulatedFetchMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const moviesFromData = await movieAPI.getMovies(); // retorna API totalmente tratada.
+        this.setState({
+          loading: false,
+          movies: moviesFromData,
+        });
+      },
+    );
+  }
+
+  renderMovieList() {
+    const { movies } = this.state;
 
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <p>{ loading ? <Loading /> : this.renderMovieList() }</p>
     );
   }
 }
