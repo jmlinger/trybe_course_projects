892aca4c731736ce7dd67a85606abde22b6525ff
jmlinger <jmlinger10@gmail.com>
2021-07-08 12:34:38 -0300

    Resolve requisito 1: criou-se rotas para todas as paginas em App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { render } from 'enzyme';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
-}
+import NotFound from './pages/NotFound';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
 
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 export default App;
