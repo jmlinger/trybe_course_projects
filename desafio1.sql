fa41402db34c8d3ca446a5be0c66777656f7231d
jmlinger <jmlinger10@gmail.com>
2021-10-01 20:06:52 -0300

    Resolve requsito 1: resolve erros, tentativa 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,8 @@
-CREATE DATABASE IF NOT EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
 
 CREATE TABLE planos (
     plano_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -8,9 +12,9 @@ CREATE TABLE planos (
 
 INSERT INTO planos (plano_id, plano, valor)
 VALUES
-	(1, 'gratuito', 0),
-	(2, 'familiar', 7.99),
-	(3, 'universitário', 5.99);
+(1, 'gratuito', 0),
+(2, 'familiar', 7.99),
+(3, 'universitário', 5.99);
 
 CREATE TABLE usuarios (
     usuario_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -23,10 +27,10 @@ CREATE TABLE usuarios (
 
 INSERT INTO usuarios (usuario_id, nome, idade, plano_id)
 VALUES 
-	(1, 'Thati', 23, 1),
-	(2, 'Cintia', 35, 2),
-    (3, 'Bill', 35, 3),
-	(4, 'Roger', 35, 1);
+(1, 'Thati', 23, 1),
+(2, 'Cintia', 35, 2),
+(3, 'Bill', 35, 3),
+(4, 'Roger', 35, 1);
 
 CREATE TABLE artistas (
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -35,10 +39,10 @@ CREATE TABLE artistas (
 
 INSERT INTO artistas (artista_id, artista)
 VALUES 
-	(1, 'Walter Phoenix'),
-	(2, 'Peter Strong'),
-	(3, 'Lance Day'),
-	(4, 'Freedie Shannon');
+(1, 'Walter Phoenix'),
+(2, 'Peter Strong'),
+(3, 'Lance Day'),
+(4, 'Freedie Shannon');
 
 CREATE TABLE albums (
     album_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -50,11 +54,11 @@ CREATE TABLE albums (
 
 INSERT INTO albums (album_id, titulo, artista_id) 
 VALUES
-	(1, 'Envious', 1),
-	(2, 'Exuberant', 1),
-	(3, 'Hallowed Steam', 2),
-	(4, 'Incandescent', 3),
-	(5, 'Temporary Culture', 4);
+(1, 'Envious', 1),
+(2, 'Exuberant', 1),
+(3, 'Hallowed Steam', 2),
+(4, 'Incandescent', 3),
+(5, 'Temporary Culture', 4);
 
 CREATE TABLE cancoes (
     cancao_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -66,24 +70,24 @@ CREATE TABLE cancoes (
 
 INSERT INTO cancoes (cancao_id, titulo, album_id)
 VALUES
-	(1, 'Soul For Us', 1),
-	(2, 'Reflections Of Magic', 1),
-	(3, 'Dance With Her Own', 1),
-	(4, 'Troubles Of My Inner Fire', 2),
-	(5, 'Time Fireworks', 2),
-	(6, 'Magic Circus', 3),
-	(7, 'Honey, So Do I', 3),
-	(8, "Sweetie, Let's Go Wild", 3),
-	(9, 'She Knows', 3),
-	(10, 'Fantasy For Me', 4),
-	(11, 'Celebration Of More', 4),
-	(12, 'Rock His Everything', 4),
-	(13, 'Home Forever', 4),
-	(14, 'Diamond Power', 4),
-	(15, "Honey, Let's Be Silly", 4),
-	(16, 'Thang Of Thunder', 5),
-	(17, 'Words Of Her Life', 5),
-	(18, 'Without My Streets', 5);
+(1, 'Soul For Us', 1),
+(2, 'Reflections Of Magic', 1),
+(3, 'Dance With Her Own', 1),
+(4, 'Troubles Of My Inner Fire', 2),
+(5, 'Time Fireworks', 2),
+(6, 'Magic Circus', 3),
+(7, 'Honey, So Do I', 3),
+(8, "Sweetie, Let's Go Wild", 3),
+(9, 'She Knows', 3),
+(10, 'Fantasy For Me', 4),
+(11, 'Celebration Of More', 4),
+(12, 'Rock His Everything', 4),
+(13, 'Home Forever', 4),
+(14, 'Diamond Power', 4),
+(15, "Honey, Let's Be Silly", 4),
+(16, 'Thang Of Thunder', 5),
+(17, 'Words Of Her Life', 5),
+(18, 'Without My Streets', 5);
 
 CREATE TABLE historico_de_reproducoes (
     usuario_id INT NOT NULL,
@@ -96,20 +100,20 @@ CREATE TABLE historico_de_reproducoes (
 
 INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
 VALUES
-	(1, 1),
-	(1, 6),
-	(1, 14),
-	(1, 16),
-	(2, 13),
-	(2, 17),
-	(2, 2),
-	(2, 15),
-	(3, 4),
-	(3, 16),
-	(3, 6),
-	(4, 3),
-	(4, 18),
-	(4, 11);
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
 
 CREATE TABLE seguindo_artistas (
     usuario_id INT NOT NULL,
@@ -122,11 +126,11 @@ CREATE TABLE seguindo_artistas (
 
 INSERT INTO seguindo_artistas (usuario_id, artista_id)
 VALUES
-	(1, 1),
-	(1, 4),
-	(1, 3),
-	(2, 1),
-	(2, 3),
-	(3, 2),
-	(3, 1),
-	(4, 4);
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
