20ef5b2a2aaf1dce2312b4dcdcb4735d3c413141
jmlinger <jmlinger10@gmail.com>
2021-10-02 13:05:53 -0300

    Resolve requisito 2: cria view que exibe a quantidade total de artistas, albuns e cancoes

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+    COUNT(CAN.cancao_id) AS cancoes,
+    COUNT(ART.artista_id) AS artistas,
+    COUNT(ALB.album_id) AS albuns
+FROM
+    cancoes AS CAN
+INNER JOIN
+    artistas AS ART ON ART.artista_id = CAN.artista_id
+INNER JOIN
+    albuns AS ALB ON ALB.album_id = CAN.album_id;
