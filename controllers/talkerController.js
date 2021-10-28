52df4d1b44fb359d5f5b3cb557f633992c71b35e
jmlinger <jmlinger10@gmail.com>
2021-10-28 16:04:21 -0300

    Resolve requisito 2: criou-se o endpoint GET /talker/:id.

--- controllers/talkerController.js
+++ controllers/talkerController.js
@@ -1,10 +1,26 @@
 const { readJSON } = require('../utilities/JSONHandlers');
 
-const talkerJSON = './talker.json';
+const talkersJSON = './talker.json';
+
+// /**
+//  * @type {IRouter RequestHandler}
+//  */
 
 const readAll = async (_req, res) => {
-  const talker = await readJSON(talkerJSON);
-  return res.status(200).json(talker);
+  const talkers = await readJSON(talkersJSON);
+  return res.status(200).json(talkers);
 };
 
-module.exports = { readAll };
+const readById = async (req, res, next) => {
+  const { id } = req.params;
+  const talkers = await readJSON(talkersJSON);
+  const talkerById = talkers.find((talker) => talker.id === Number(id)); // o id vem da url como string!
+
+  if (!talkerById) {
+    next('Pessoa palestrante não encontrada');
+  }
+
+  return res.status(200).json(talkerById);
+};
+
+module.exports = { readAll, readById };
--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const rescue = require('express-rescue');
-const { readAll } = require('../controllers/talkerController');
+const { 
+  readAll,
+  readById,
+} = require('../controllers/talkerController');
 
 const router = express.Router();
 
 router.get('/talker',
   rescue(readAll));
+
+router.get('/talker/:id', 
+  rescue(readById),
+  (err, _req, res, _next) => 
+    res.status(404).json({ message: `${err}` }));
 
 module.exports = router;
