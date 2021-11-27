b6fa5839f567a619220fe77b9607d8401713075d
jmlinger <jmlinger10@gmail.com>
2021-11-26 22:03:03 -0300

    subindo para o pc!

--- model/entity/findById.js
+++ model/entity/findById.js
@@ -3,6 +3,7 @@ const connection = require('../connection');
 
 module.exports = async (collection, id) => {
   const db = await connection();
+
   return db.collection(collection).findOne(
     { _id: ObjectId(id) },
   );
