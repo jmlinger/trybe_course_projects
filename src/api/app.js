f554e449d4eb4bba9d4757bf85181407ba3a39aa
jmlinger <jmlinger10@gmail.com>
2021-12-01 12:37:17 -0300

    Iniciando o projeto.

--- src/api/app.js
+++ src/api/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
