1eb6743dabf4dbb6a58bc81f993ce9df298c2cba
jmlinger <jmlinger10@gmail.com>
2021-12-10 19:31:54 -0300

    correçao back

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
     
 const { PORT, UPSIDEDOWN_MODE } = process.env;
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'false';
 
 console.log(PORT);
 
