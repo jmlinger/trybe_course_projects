0340b3a5f81d06e76dabbd6c617a2f9b1c91dcdb
jmlinger <jmlinger10@gmail.com>
2022-02-11 12:25:19 -0300

    Resolve requisito 11

---
+++ controllers/blogPosts/remove.js
@@ -0,0 +1,11 @@
+const Services = require('../../services/blogPosts');
+
+module.exports = async (req, res, _next) => {
+  const { id: postId } = req.params;
+  const { id: userId } = req.user;
+
+  const result = await Services.remove(postId, userId);
+
+  return res.status(result.status)
+    .json(result.message ? { message: result.message } : null);
+};
--- controllers/blogPosts/router.js
+++ controllers/blogPosts/router.js
@@ -8,10 +8,12 @@ const create = require('./create');
 const list = require('./list');
 const get = require('./get');
 const update = require('./update');
+const remove = require('./remove');
 
 router.post('/', auth, rescue(create));
 router.get('/', auth, rescue(list));
 router.get('/:id', auth, rescue(get));
 router.put('/:id', auth, rescue(update));
+router.delete('/:id', auth, rescue(remove));
 
 module.exports = router;
