d4910924999e3db7d035e2931894eaf3ccd8d70d
jmlinger <jmlinger10@gmail.com>
2022-03-29 11:37:55 -0300

    test: implementa testes para a funcao sort_by.

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,18 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
+        {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-01-01"},
+        {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == 1000
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == 7000
+
+    sort_by(jobs, "date_posted")
+    assert jobs[0]["date_posted"] == "2021-01-01"
