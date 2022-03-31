54eb5fcb9313d1a652a086a2074b751ac64fc9b9
Vinicius Carvalho <vinicius.10.carvalho.100@gmail.com>
2022-03-31 19:06:19 -0300

    refactor: Na class SimpleReport foram modificadas as variaveis de camelCase para snake_case

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,39 +2,39 @@ from datetime import date, datetime
 
 
 class SimpleReport:
-    def stringToDate(param1):
+    def string_to_date(date):
         return datetime.strptime(
-            param1,
+            date,
             "%Y-%m-%d",
         ).date()
 
-    def countCompany(companyNameList):
-        countCompany = 0
-        company_name = companyNameList[0]
-        for companyName in companyNameList:
-            count = companyNameList.count(companyName)
-            if count > countCompany:
-                countCompany = count
-                company_name = companyName
+    def count_company(company_name_list):
+        count_company = 0
+        company_name = company_name_list[0]
+        for name in company_name_list:
+            count = company_name_list.count(name)
+            if count > count_company:
+                count_company = count
+                company_name = name
         return company_name
 
     @classmethod
     def generate(self, list):
-        manufacturingDate = min([item["data_de_fabricacao"] for item in list])
-        expirationDate = self.stringToDate(list[0]["data_de_validade"])
-        companyNameList = []
+        manufacturing_date = min([date["data_de_fabricacao"] for date in list])
+        expiration_date = self.string_to_date(list[0]["data_de_validade"])
+        company_name_list = []
         for item in list:
-            companyNameList.append(item["nome_da_empresa"])
-            expirationDateitem = self.stringToDate(item["data_de_validade"])
-            if date.today() < expirationDateitem < expirationDate:
-                expirationDate = expirationDateitem
+            company_name_list.append(item["nome_da_empresa"])
+            expiration_date_item = self.string_to_date(item["data_de_validade"])
+            if date.today() < expiration_date_item < expiration_date:
+                expiration_date = expiration_date_item
 
-        company_name = self.countCompany(companyNameList)
-        returnManufacturingText = 'Data de fabricação mais antiga:'
-        returnExpirationText = 'Data de validade mais próxima:'
-        mostCompanyText = 'Empresa com maior quantidade de produtos estocados:'
+        company_name = self.count_company(company_name_list)
+        manufacturing_text = 'Data de fabricação mais antiga:'
+        expiration_text = 'Data de validade mais próxima:'
+        company_Text = 'Empresa com maior quantidade de produtos estocados:'
         return (
-            f'{returnManufacturingText} {manufacturingDate}\n'
-            f'{returnExpirationText} {expirationDate}\n'
-            f'{mostCompanyText} {company_name}\n'
+            f'{manufacturing_text} {manufacturing_date}\n'
+            f'{expiration_text} {expiration_date}\n'
+            f'{company_Text} {company_name}\n'
         )
