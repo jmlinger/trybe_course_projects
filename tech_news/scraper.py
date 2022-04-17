0815c9251fb4433f0b16acb209023946b43c0d6a
jmlinger <jmlinger10@gmail.com>
2022-04-16 22:15:20 -0300

    feat: implementa uma função que retorna a url da próxima página.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css("div.tec--list__item ~ a::attr(href)").get()
+
+# print(scrape_next_page_link(fetch(base_url)))
 
 
 # Requisito 4
