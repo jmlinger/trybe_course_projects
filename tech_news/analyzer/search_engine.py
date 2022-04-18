de5bc6ecc115cd4552e217b625817bc375f32239
jmlinger <jmlinger10@gmail.com>
2022-04-18 20:35:28 -0300

    feat: implementa uma função que recebe uma fonte e retorna informações de notícias cujas fontes contenham aquela string.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -28,7 +28,10 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query = {"sources": ignore_case(source)}
+    news_list = search_news(query)
+
+    return [(news["title"], news["url"]) for news in news_list]
 
 
 # Requisito 9
