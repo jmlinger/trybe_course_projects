02617324c45c1cfb96425f98a2d5e31b2539db3e
jmlinger <jmlinger10@gmail.com>
2022-04-29 14:39:50 -0300

    refactor: refatora study_schedule para diminuir o tempo de execução, tentativa 4.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if type(target_time) != int:
-        return None
     count = 0
-    for time_in, time_out in permanence_period:
-        if (type(time_in) != int or
-                type(time_out) != int):
-            return None
-        if time_in <= target_time <= time_out:
-            count += 1
-    return count
+    try:
+        for time_in, time_out in permanence_period:
+            if time_in <= target_time <= time_out:
+                count += 1
+        return count
+    except TypeError:
+        return None
