c0c3bc2b24b31554507d6a3b845f081ac549c98c
jmlinger <jmlinger10@gmail.com>
2022-04-29 13:39:32 -0300

    feat: implementa função study_schedule.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for period in permanence_period:
+        if (type(target_time) is not int or
+                type(period[0]) is not int or
+                type(period[1]) is not int):
+            count = 0
+            break
+        if period[0] <= target_time <= period[1]:
+            count += 1
+    if count == 0:
+        return None
+    else:
+        return count
