e33405c0613fbf3382fb7951a6ca142314cd32fc
jmlinger <jmlinger10@gmail.com>
2022-04-29 17:44:50 -0300

    feat: implementa a função is_anagram.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+
+    for letter_f in first_string:
+        for letter_s in second_string:
+            if letter_f == letter_s:
+                second_string = second_string.replace(letter_s, '', 1)
+                break
+    return len(second_string) == 0
