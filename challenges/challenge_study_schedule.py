c0158ead61f9471d981b4156917989702394a2ca
jmlinger <jmlinger10@gmail.com>
2022-04-29 14:16:52 -0300

    refactor: refatora study_schedule para diminuir o tempo de execução.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,9 @@
 def study_schedule(permanence_period, target_time):
+    if type(target_time) is not int:
+        return None
     count = 0
     for period in permanence_period:
-        if (type(target_time) is not int or
-                type(period[0]) is not int or
+        if (type(period[0]) is not int or
                 type(period[1]) is not int):
             return None
         if period[0] <= target_time <= period[1]:
