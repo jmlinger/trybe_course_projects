77aff4ecea7772aa46ccac3b430f6917b76c9f9c
jmlinger <jmlinger10@gmail.com>
2022-04-29 14:12:56 -0300

    refactor: refatora study_schedule.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,11 +4,7 @@ def study_schedule(permanence_period, target_time):
         if (type(target_time) is not int or
                 type(period[0]) is not int or
                 type(period[1]) is not int):
-            count = 0
-            break
+            return None
         if period[0] <= target_time <= period[1]:
             count += 1
-    if count == 0:
-        return None
-    else:
-        return count
+    return count
