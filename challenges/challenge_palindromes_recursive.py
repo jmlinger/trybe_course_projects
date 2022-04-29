300aff67b6cd855f446fb643a31f06e4dee36c16
jmlinger <jmlinger10@gmail.com>
2022-04-29 16:28:08 -0300

    feat: implementa a função is_palindrome_recursive.

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    elif high_index <= low_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
