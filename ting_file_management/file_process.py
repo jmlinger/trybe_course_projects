73caed40fd0ab5b15e265505b5de3c6053cb1f8f
jmlinger <jmlinger10@gmail.com>
2022-05-17 13:27:21 -0300

    feat: implementa uma função process dentro do módulo file_process capaz de ler o arquivo carregado na função anterior e efetuar o preprocessamento do conteúdo.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_lines_list = txt_importer(path_file)
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_lines_list),
+        "linhas_do_arquivo": file_lines_list
+    }
+
+    if all(info["nome_do_arquivo"] != path_file for info in instance.data):
+        instance.enqueue(file_info)
+
+    sys.stdout.write(f"{file_info}")
 
 
 def remove(instance):
